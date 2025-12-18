a9ca6c066bfaf3dfeb57f77f51b009f2d277791a
icaromartins <34662219+Icaro-Martins@users.noreply.github.com>
2025-12-18 16:40:39 -0300

    Add files via upload

---
+++ studies/001 - fundamestos.py
@@ -0,0 +1,16 @@
+# Isto é um comentario.
+# Isto é outro comentario.
+
+print("Olá Mundo!") # Isto é um comentario.
+
+
+"""Comentario com multiplas
+linhas usando 3 apas,
+com string de multiplas linhas, ele não executa."""
+
+print(10)
+print(10.5)
+
+print("Maça","Pera","Uva", sep="-")
+
+print("Maça","Pera","Uva ", end="Fim")
